7676dfe3e3f26668c6f24b8edee1f2a19ef38ee8
Brian <bstanton773@gmail.com>
2024-05-31 12:33:27 -0500

    Add production migrations (for Postgresql because MySQL was breaking)

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
+
+MIGRATIONS_FOLDER = os.environ.get('MIGRATIONS_FOLDER') or 'migrations'
 
 class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
-migrate = Migrate()
+migrate = Migrate(directory=MIGRATIONS_FOLDER)
